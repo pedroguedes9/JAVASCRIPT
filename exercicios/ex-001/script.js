77b99e251324a577feacf7c011e87bd7877d3798
Pedro Guedes <160842810+pedroguedes9@users.noreply.github.com>
2024-07-15 20:08:48 -0300

    primeiras aulas e comandos

---
+++ exercicios/ex-001/script.js
@@ -0,0 +1,8 @@
+let carro = "ferrrari"
+console.log(carro)
+
+let precoBolo = 19.99
+console.log(precoBolo)
+
+let cidade = "São Paulo"
+console.log(cidade)
---
+++ exercicios/ex-002/script.js
@@ -0,0 +1,5 @@
+let nome = "Pedro"
+let idade = 90
+
+console.log(nome)
+console.log(idade)
