2e6b25dd9927d4905e596f6c1067b860b14e6261
Pedro Guedes <160842810+pedroguedes9@users.noreply.github.com>
2024-07-16 18:00:00 -0300

    Update script.js

--- exercicios/ex-002/script.js
+++ exercicios/ex-002/script.js
@@ -1,5 +1,47 @@
-var nome = "pedro"
-console.log(nome)
+let idade = 19
+if (idade >= 18) {
+    console.log("Você é maior de idade, pode passar")
+}
+else {
+    console.log("Você é menor de idade, não pode passar.")
+}
 
-nome = "joao"
-console.log(nome)
+
+
+
+
+let preco = 1
+if (preco >= 50) {
+    console.log("VocÊ pode comprar")
+}
+else {
+    console.log("você nao pode comprar")
+}
+
+
+
+
+
+let numero = 50
+if (numero > 0) {
+    console.log("esse numero é positivo")
+}
+else if (numero == 0) {
+    console.log("esse numero é nulo")
+}
+else {
+    console.log("esse numero é negativo")
+}
+
+
+
+
+
+let numeroSenha = "12345"
+let senha = "12345"
+if (numeroSenha == senha) {
+    console.log("acesso permitido")
+}
+else {
+    console.log("acesso negado")
+}
