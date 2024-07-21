1292882f4d244cd27d12e58aa58d444e927b6e56
Pedro Guedes <160842810+pedroguedes9@users.noreply.github.com>
2024-07-21 13:40:29 -0300

    array

---
+++ exercicios/ex-007/script.js
@@ -0,0 +1,16 @@
+let colors = ["blue", "green", "red"]
+console.log(colors[2])
+
+let ages = [10, 20, 30]
+console.log(ages[1])
+
+let list = ["aldjfo", "ffds", 2, colors[1]]
+console.log(list[3])
+
+let lista = ["adkjflaj", ["xx", "yy"]]
+console.log(lista[0][0])
+
+console.log(list[0])
+
+console.log(list[1],list[0])
+console.log(ages)
