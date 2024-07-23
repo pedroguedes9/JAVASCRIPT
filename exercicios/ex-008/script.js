5d0aa1badde04e4f9fe73a1554eaa864605f6890
Pedro Guedes <160842810+pedroguedes9@users.noreply.github.com>
2024-07-23 18:22:17 -0300

    Loop for

--- exercicios/ex-008/script.js
+++ exercicios/ex-008/script.js
@@ -1,9 +1,13 @@
-let pessoa = {
-    nome: "pedro",
-    idade: 16,
-    sobrenome: "guedes",
-    nomeCompleto: function() {
-        return this.nome + this.sobrenome
-    }
+let cores = ["preto", "branco", "azul", "vermelho"]
+cores.push("verde")
+for(let n = 0; n < cores.length; n++) {
+    console.log(cores[n])
 }
-console.log(pessoa.nomeCompleto())
+
+for(let i in cores) {
+    console.log(cores[i])
+}
+
+for(let cor of cores) {
+    console.log(cor)
+}
