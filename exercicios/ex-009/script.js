cd61b25bc0f91e3c2f5dcc0214cbfb47cc99b1f4
Pedro Guedes <160842810+pedroguedes9@users.noreply.github.com>
2024-07-27 14:17:03 -0300

    .

--- exercicios/ex-009/script.js
+++ exercicios/ex-009/script.js
@@ -5,3 +5,20 @@ if(fruits.includes("uva")) {
 else {
     console.log("A fruta uva não está na lista.")
 }
+
+let fruitsM = fruits.map(function (fruit){
+    return fruit.toUpperCase()
+})
+console.log(fruitsM)
+
+let numbers = [1, 2, 3, 4, 5]
+let sumNumbers = numbers.reduce(function (valorAcumulado, valorAtual) {
+    return valorAcumulado + valorAtual
+} )
+console.log(sumNumbers)
+
+let numList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]   
+let firstNumberGreaterThanFive = numList.find(function (number) {
+    return number > 5
+})
+console.log(firstNumberGreaterThanFive)
