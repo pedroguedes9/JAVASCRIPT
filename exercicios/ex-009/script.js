42f17fdab5db6da463b3574e25fa8a010d8c3e8c
Pedro Guedes <160842810+pedroguedes9@users.noreply.github.com>
2024-08-03 15:55:29 -0300

    trabalhdno com classes

--- exercicios/ex-009/script.js
+++ exercicios/ex-009/script.js
@@ -1,12 +1,13 @@
 const inputText = document.querySelector('#itexto')
 function clicou() {
-    if (inputText.getAttribute("style") === "background-color: blue;") {
-        inputText.style.backgroundColor = "red"
+    if(inputText.classList.contains("azul")) {
+        inputText.classList.replace("azul", "vermelho")
     }
     else {
-        inputText.style.backgroundColor = "blue"
+        inputText.classList.replace("vermelho", "azul")
     }
+    console.log(inputText.classList)
 }
 document.querySelector("#button").addEventListener('click', clicou)
-console.log(inputText.getAttribute("style"))
+
 
