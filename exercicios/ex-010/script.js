6860e43870ea8563c2058eae7c5f4c2a5c3ba86f
Pedro Guedes <160842810+pedroguedes9@users.noreply.github.com>
2024-08-09 18:48:34 -0300

    terminnei classes

--- exercicios/ex-010/script.js
+++ exercicios/ex-010/script.js
@@ -1,30 +1,17 @@
 class Person {
     // variáveis comuns
-    #age = 0
-    steps = 0
+    age = 0
     //construtor
-    constructor(firstName, lastName) {
-        this.firstName = firstName
-        this.lastName = lastName
+    constructor(name) {
+        this.name = name
     }
     //ações
-    takeAStep(){
-        this.steps++
-    }
-    get fullName() {
-        return `${this.firstName} ${this.lastName}`
-    }
-    get age() {
-        return this.#age
-    }
-    set age(x) {
-        if(typeof x == "number") {
-            this.#age = x
-        }
-    }
 }
-let p1 = new Person("Pedro", "Guedes")
-let p2 = new Person("Luiz", "Pernambuco")
-p1.takeAStep()
-p1.age = 30
-console.log(`Idade é ${p1.age}`)
+
+function createPerson(name, age) {
+    let p = new Person(name)
+    p.age = age
+    return p
+}
+let p1 = createPerson("Pedro", 16)
+console.log(p1)
