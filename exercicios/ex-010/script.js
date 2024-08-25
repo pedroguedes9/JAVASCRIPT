7384cdd9561346ebdcfcf4abb377b660dbc9b6d9
Pedro Guedes <160842810+pedroguedes9@users.noreply.github.com>
2024-08-25 15:15:12 -0300

    Update script.js

--- exercicios/ex-010/script.js
+++ exercicios/ex-010/script.js
@@ -1,10 +1,18 @@
-function createStudent (name, school, year, age) {
-    return {
-        name, 
-        school,
-        year,
-        age,
-    }
+const defaultUser = {
+    name: "",
+    email: "",
+    level: 1
 }
-let student1 = createStudent("pedro", "elite", 2, 16) 
-console.log(student1)
+const user1 = {
+    ...defaultUser,
+    name: "pedro",
+    email: "skdkfjakj",
+}
+
+const adm1 = {
+    ...defaultUser,
+    name: "guedes",
+    email: "adm",
+    level: 2
+}
+console.log(user1, adm1)
