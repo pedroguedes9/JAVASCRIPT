91e5d33c0a2c63f1014a0df2bd41457745d9e0a7
Pedro Guedes <160842810+pedroguedes9@users.noreply.github.com>
2024-08-25 14:36:19 -0300

    Update script.js

--- exercicios/ex-010/script.js
+++ exercicios/ex-010/script.js
@@ -1,17 +1,10 @@
-class Person {
-    // variáveis comuns
-    age = 0
-    //construtor
-    constructor(name) {
-        this.name = name
+function createStudent (name, school, year, age) {
+    return {
+        name, 
+        school,
+        year,
+        age,
     }
-    //ações
 }
-
-function createPerson(name, age) {
-    let p = new Person(name)
-    p.age = age
-    return p
-}
-let p1 = createPerson("Pedro", 16)
-console.log(p1)
+let student1 = createStudent("pedro", "elite", 2, 16) 
+console.log(student1)
