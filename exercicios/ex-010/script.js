8f7582a4c8ad630095895533decd6d14a927bf37
Pedro Guedes <160842810+pedroguedes9@users.noreply.github.com>
2024-09-03 20:03:27 -0300

    promises

--- exercicios/ex-010/script.js
+++ exercicios/ex-010/script.js
@@ -3,11 +3,18 @@ function clicou() {
     fetch("https://jsonplaceholder.typicode.com/posts")
         .then((response) => {
             return response.json()
+            
         })
         .then((data) => {
             title = data[0].title
             document.querySelector("h1").innerText = title
         })
-        
+        .catch((error) => {
+            console.log("deu problema")
+            console.log(error)
+        })
+        .finally(() => {
+            alert("afdkfj")
+        })
 }
 document.querySelector("#button").addEventListener("click", clicou)
