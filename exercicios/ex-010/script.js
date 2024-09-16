6b9fbc4d1e64b6cf2acc980f919f9549f1b7e01d
Pedro Guedes <160842810+pedroguedes9@users.noreply.github.com>
2024-09-16 16:07:59 -0300

    métodos de requisição

--- exercicios/ex-010/script.js
+++ exercicios/ex-010/script.js
@@ -19,4 +19,26 @@ function clicou() {
             alert("afdkfj")
         })
 }
+function inserir() {
+    fetch("https://jsonplaceholder.typicode.com/posts",
+        {
+            method: "post",
+            headers: {
+                "Content-Type" : "application/json"
+            },
+            body: JSON.stringify({
+                title: "titulo de teste",
+                body: "corpo de teste,",
+                userId: 2
+            })
+        }
+    )
+    .then((response) => {
+        return response.json()
+    })
+    .then((json) => {
+        console.log(json)
+    })
+}
 document.querySelector("#button").addEventListener("click", clicou)
+document.querySelector("#inserir").addEventListener("click", inserir)
