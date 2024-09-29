9af4b6c0b0c451edd1d265911af8a5bfab69995c
Pedro Guedes <160842810+pedroguedes9@users.noreply.github.com>
2024-09-29 09:15:46 -0300

    Update script.js

--- exercicios/ex-010/script.js
+++ exercicios/ex-010/script.js
@@ -1,11 +1,2 @@
-let lista = [
-    {id:1, nome:"pedro"},
-    {id:2, nome:"chaves"},
-    {id:3, nome:"guedes"}
-]
-let lista2 = []
-lista2 = lista.find(function(item) {
-    return item.nome == "pedro"
-})
-console.log(lista2)
+
 
