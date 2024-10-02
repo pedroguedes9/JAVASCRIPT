91d7b78c0023a1ccf2d8182fa455a1bd81c733ab
Pedro Guedes <160842810+pedroguedes9@users.noreply.github.com>
2024-10-02 20:50:57 -0300

    intervalos

--- exercicios/ex-010/script.js
+++ exercicios/ex-010/script.js
@@ -1,2 +1,14 @@
-let novoValor = Math.min()
-console.log(novoValor)
+document.querySelector("#começar").addEventListener("click", rodar)
+document.querySelector("#parar").addEventListener("click", parar)
+let timer 
+function rodar () {
+    timer = setTimeout( function() {
+        document.querySelector(".demo").innerHTML = "rodou"
+    },
+    2000
+)}
+
+function parar () {
+    clearTimeout(timer)
+}
+
