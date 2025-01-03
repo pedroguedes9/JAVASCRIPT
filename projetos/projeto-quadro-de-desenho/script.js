d2486acb35bacaca055c65f78cb2022653a84681
Pedro Guedes <160842810+pedroguedes9@users.noreply.github.com>
2025-01-03 14:15:29 -0300

    .

--- projetos/projeto-quadro-de-desenho/script.js
+++ projetos/projeto-quadro-de-desenho/script.js
@@ -1,8 +1,12 @@
 //inicial data
 let currentColor = "black"
+let canDraw = false
+let mouseX = 0
+let mouseY = 0
+
 let screen = document.querySelector("#tela")
 let context = screen.getContext("2d")
-let canDraw = false
+const canvasDownload = document.querySelector("#canvasDownload")
 
 
 //events
@@ -12,6 +16,7 @@ document.querySelectorAll(".colorArea .color").forEach(item => {
 screen.addEventListener("mousedown", mouseDownEvent)
 screen.addEventListener("mousemove", mouseMoveEvent)
 screen.addEventListener("mouseup", mouseUpEvent)
+document.querySelector(".clear").addEventListener("click", clearScreen)
 
 //functions
 function colorClickEvent(e) {
@@ -20,16 +25,41 @@ function colorClickEvent(e) {
     document.querySelector(".color.active").classList.remove("active")
     e.target.classList.add("active")
 }
-function mouseDownEvent () {
+function mouseDownEvent (e) {
     canDraw = true
+    mouseX = e.pageX - screen.offsetLeft
+    mouseY = e.pageY - screen.offsetTop
 }
 function mouseMoveEvent(e) {
     if(canDraw) {
-        let pointX = e.pageX - screen.offsetLeft
-        let pointY = e.pageY - screen.offsetB
-        console.log(pointX, pointY)
+        draw(e.pageX, e.pageY)
+        
     }
 }
 function mouseUpEvent() {
     canDraw = false
+}
+
+function draw(x,y) {
+    let pointX = x - screen.offsetLeft
+    let pointY = y - screen.offsetTop
+
+    context.beginPath()
+    context.lineWidth = 5
+    context.lineJoin = "round"
+    context.moveTo(mouseX, mouseY)
+    context.lineTo(pointX, pointY)
+    context.closePath()
+    context.strokeStyle = currentColor
+    context.stroke()
+
+    mouseX = pointX
+    mouseY = pointY
+    const pngDataUrl = screen.toDataURL("image/png")
+    canvasDownload.href = pngDataUrl
+}
+
+function clearScreen() {
+    context.setTransform(1, 0, 0, 1, 0, 0)
+    context.clearRect(0, 0, context.canvas.width, context.canvas.height)
 }
